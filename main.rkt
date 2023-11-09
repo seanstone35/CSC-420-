b425fd38568887269d5f0579ab2549bed5ebe826
seanstone35 <82997745+seanstone35@users.noreply.github.com>
2023-11-09 11:56:50 -0500

    Add files via upload
    Added  output-expression and while-expression to the language.

---
+++ main.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(require "Utility.rkt")
+(require "Parser.rkt")
+(require "Processor.rkt")
+
+(define var_env
+  '(;environment
+   (;global variable scope
+    (a 1) (b 2) (c 3)
+    )
+   )
+  )
+
+(define code '(when (< a 4) ((assign a (+ a 1)) (out a)))) 
+(define parsed (parser code))
+parsed
+(processor parsed var_env)
+
